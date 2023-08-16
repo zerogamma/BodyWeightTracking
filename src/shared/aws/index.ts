74dc5e18427f4fde8c0bdb385cd9a160793c570c
jin <zeroalpha2000@gmail.com>
2023-08-16 02:01:11 -0300

    old access

--- src/shared/aws/index.ts
+++ src/shared/aws/index.ts
@@ -1,10 +1,10 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-// import { fromEnv } from '@aws-sdk/credential-providers';
+import { fromEnv } from '@aws-sdk/credential-providers';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 export const awsClient = DynamoDBDocumentClient.from(
   new DynamoDBClient({
     region: process.env.AMPLIFY_STORAGE_REGION,
-    // credentials: fromEnv(),
+    credentials: fromEnv(),
   })
 );

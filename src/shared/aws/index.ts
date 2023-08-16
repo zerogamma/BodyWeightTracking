7471d5d2c444bbec35e1a66b95af84ba5cfffeb3
jin <zeroalpha2000@gmail.com>
2023-08-16 19:07:59 -0300

    anda?

--- src/shared/aws/index.ts
+++ src/shared/aws/index.ts
@@ -10,15 +10,16 @@ const logger = new Logger('AWSDYNAMO');
 const getSettings = async () => {
   logger.info('loading credential');
   const credentials = await Auth.currentCredentials();
-  logger.info('current credential', credentials);
-  const creds = Auth.essentialCredentials(credentials);
+  //   logger.info('current credential', credentials);
+  //   const creds = Auth.essentialCredentials(credentials);
   //   AWS.config.credentials = new AWS.Credentials(creds.accessKeyId, creds.secretAccessKey, creds.sessionToken);
 
-  logger.debug('credential', creds);
+  //   logger.debug('credential', creds);
 
   return new DynamoDBClient({
     region: process.env.AMPLIFY_STORAGE_REGION as string,
     apiVersion: '2012-08-10',
+    credentials: credentials,
     // credentials: Auth.essentialCredentials(credentials),
   });
 };

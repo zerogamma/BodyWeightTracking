f3ee2b5b3671c60fcd42719752b6b91f1c556b6e
jin <zeroalpha2000@gmail.com>
2023-08-16 02:08:46 -0300

    force credential

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ const nextConfig = {
   env: {
     AMPLIFY_STORAGE_TABLES: '{ "BodyInfo": "BodyInfo" }',
     AMPLIFY_STORAGE_REGION: 'us-east-1',
+    AWS_ACCESS_KEY_ID: 'AKIAYHFP3ONZT4TFUBHD',
+    AWS_SECRET_ACCESS_KEY: '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',
   },
 };
 
--- src/shared/aws/index.ts
+++ src/shared/aws/index.ts
@@ -1,10 +1,12 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { fromEnv } from '@aws-sdk/credential-providers';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
 export const awsClient = DynamoDBDocumentClient.from(
   new DynamoDBClient({
-    region: process.env.AMPLIFY_STORAGE_REGION,
-    credentials: fromEnv(),
+    region: process.env.AMPLIFY_STORAGE_REGION as string,
+    credentials: {
+      accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string,
+    },
   })
 );

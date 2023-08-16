7ebdd2f9870d09be5722ca82790b08dab8b04d32
jin <zeroalpha2000@gmail.com>
2023-08-16 16:29:45 -0300

    auth

--- src/shared/aws/index.ts
+++ src/shared/aws/index.ts
@@ -1,17 +1,19 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
-import { Amplify, Auth } from 'aws-amplify';
+import { Amplify, Auth, Logger } from 'aws-amplify';
 import AWS from 'aws-sdk';
 import awsExports from '../../aws-exports';
 
 Amplify.configure({ ...awsExports, ssr: true });
+
+const logger = new Logger('AWSDYNAMO');
 
 const getSettings = async () => {
   const credentials = await Auth.currentCredentials();
   const creds = Auth.essentialCredentials(credentials);
   AWS.config.credentials = new AWS.Credentials(creds.accessKeyId, creds.secretAccessKey, creds.sessionToken);
 
-  //   console.log(Auth.essentialCredentials(credentials));
+  logger.debug('credential', creds);
 
   return new DynamoDBClient({
     region: process.env.AMPLIFY_STORAGE_REGION as string,

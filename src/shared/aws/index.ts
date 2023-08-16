d94b45335a58e4620891e4f08a974b10dc59b191
jin <zeroalpha2000@gmail.com>
2023-08-16 01:12:43 -0300

    test identity

--- src/shared/aws/index.ts
+++ src/shared/aws/index.ts
@@ -1,12 +1,12 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { fromCognitoIdentity } from '@aws-sdk/credential-providers';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
-// const credentials = {
-//   region: configuration.aws.region,
-//   credentials: {
-//     accessKeyId: configuration.aws.accessKeyId,
-//     secretAccessKey: configuration.aws.secretAccessKey,
-//   },
-// };
+const credentials = {
+  credentials: fromCognitoIdentity({
+    identityId: 'arn:aws:amplify:us-east-1:565157000051:apps/d19tcmdm1t8i2u',
+    customRoleArn: 'arn:aws:iam::565157000051:role/amplifyconsole-backend-role',
+  }),
+};
 
-export const awsClient = DynamoDBDocumentClient.from(new DynamoDBClient({}));
+export const awsClient = DynamoDBDocumentClient.from(new DynamoDBClient(credentials));

f441e59cec9d8e595ec44468235127a4148654b5
jin <zeroalpha2000@gmail.com>
2023-08-16 01:32:53 -0300

    env credential

--- src/shared/aws/index.ts
+++ src/shared/aws/index.ts
@@ -1,12 +1,9 @@
 import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { fromCognitoIdentity } from '@aws-sdk/credential-providers';
+import { fromEnv } from '@aws-sdk/credential-providers';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 
-const credentials = {
-  credentials: fromCognitoIdentity({
-    identityId: 'arn:aws:amplify:us-east-1:565157000051:apps/d19tcmdm1t8i2u',
-    customRoleArn: 'arn:aws:iam::565157000051:role/amplifyconsole-backend-role',
-  }),
-};
-
-export const awsClient = DynamoDBDocumentClient.from(new DynamoDBClient(credentials));
+export const awsClient = DynamoDBDocumentClient.from(
+  new DynamoDBClient({
+    credentials: fromEnv(),
+  })
+);
